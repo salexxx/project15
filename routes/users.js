89dbf732d81f706d8ef12044d4264f7c1f7d76fe
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-07-31 17:18:01 +0700

    валидировал запросы в роуте users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const userRout = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getUsers, getUser } = require('../controllers/user');
 
 userRout.get('/', getUsers);
-userRout.get('/:id', getUser);
+
+userRout.get('/:id', celebrate({
+    params: Joi.object().keys({
+        id: Joi.string().length(24).hex(),
+    }),
+}),getUser);
 module.exports = userRout;
