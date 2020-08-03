a448136226e743970d55223bbe049a66350ab50e
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-03 14:01:17 +0700

    +валидация Joi роута signin signup +исправил ошибка eslint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 app.use(bodyParser.json());
@@ -36,8 +37,25 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  login);
+app.post('/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required(),
+    }),
+  }),
+  createUser);
 app.use(auth);
 app.use('/users', userRout);
 app.use('/cards', cardRout);
@@ -63,5 +81,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
